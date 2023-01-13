28a7de402c3091eea0919c9b4c85537e3d9f7281
Yuliia Kolomiiets <jk182013@gmail.com>
2023-01-13 09:55:51 +0100

    feat: affichage de la liste des taches

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -930,6 +930,38 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var bootstrap_dist_css_bootstrap_min_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! bootstrap/dist/css/bootstrap.min.css */ "./node_modules/bootstrap/dist/css/bootstrap.min.css");
 
 
+const ulContainer = document.querySelector('ul');
+const todos = [{
+  text: "Aller faire les courses",
+  done: false
+}, {
+  text: "Aller chercher les resultats cliniques",
+  done: true
+}];
+
+// fonction flechee qui va creer et remplir une balise <li>
+const createTodoElement = todo => {
+  const li = document.createElement("li");
+  li.classList.add("d-flex", "align-items-start");
+  li.innerHTML = `
+        <span class="todo ${todo.done ? 'done' : ''}></span>
+        <p class="w-100">${todo.text}</p>
+        <button class="btn btn-primary mx-2">Editer</button>
+        <button class="btn btn-danger mx-2">Supprimer</button>
+    `;
+  return li;
+};
+
+// fonction flechee qui va afficher la liste des taches, map => array, filter => array, reduce => valeur
+
+const displayTodos = () => {
+  const todosNode = todos.map((todo, index) => {
+    return createTodoElement(todo);
+  });
+  ulContainer.innerHTML = '';
+  ulContainer.append(...todosNode);
+};
+displayTodos();
 })();
 
 /******/ })()
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ulContainer = document.querySelector('ul');
+
+const todos = [
+    {text: "Aller faire les courses", done: false},
+    {text: "Aller chercher les resultats cliniques", done: true}
+]
+
+// fonction flechee qui va creer et remplir une balise <li>
+const createTodoElement = (todo) => {
+    const li = document.createElement("li");
+    li.classList.add("d-flex", "align-items-start");
+    li.innerHTML = `
+        <span class="todo ${todo.done ? 'done' : ''}></span>
+        <p class="w-100">${todo.text}</p>
+        <button class="btn btn-primary mx-2">Editer</button>
+        <button class="btn btn-danger mx-2">Supprimer</button>
+    `
+
+    return li;
+}
+
+// fonction flechee qui va afficher la liste des taches, map => array, filter => array, reduce => valeur
+
+const displayTodos = () => {
+    const todosNode = todos.map((todo, index) => {
+        return createTodoElement(todo)
+    });
+
+    ulContainer.innerHTML = '';
+    ulContainer.append(...todosNode)
+}
+
+displayTodos();
