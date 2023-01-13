ce4269ae07a6b126ffd8690965a3d3d0eab700ea
Yuliia Kolomiiets <jk182013@gmail.com>
2023-01-13 10:47:42 +0100

    feat: ajout une tache

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -931,6 +931,8 @@ __webpack_require__.r(__webpack_exports__);
 
 
 const ulContainer = document.querySelector('ul');
+const form = document.querySelector("form");
+const input = document.querySelector("form > input");
 const todos = [{
   text: "Aller faire les courses",
   done: false
@@ -938,10 +940,21 @@ const todos = [{
   text: "Aller chercher les resultats cliniques",
   done: true
 }];
+form.addEventListener('submit', event => {
+  event.preventDefault(); // on arrete le rechargement de la page
+  const todoText = input.value; // je recupere le text dans le zone de saisie du input
+  input.value = '';
+  const newTodo = {
+    text: todoText,
+    done: false
+  };
+  todos.push(newTodo); // on ajoute cette nouvelle tache dans la liste des taches todos
+  displayTodos();
+});
 
 // fonction flechee qui va creer et remplir une balise <li>
 const createTodoElement = todo => {
-  const li = document.createElement("li");
+  const li = document.createElement('li');
   li.classList.add("d-flex", "align-items-start");
   li.innerHTML = `
         <span class="todo ${todo.done ? 'done' : ''}></span>
--- src/index.js
+++ src/index.js
@@ -3,15 +3,28 @@ import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ulContainer = document.querySelector('ul');
+const form = document.querySelector("form");
+const input = document.querySelector("form > input");
 
 const todos = [
     {text: "Aller faire les courses", done: false},
-    {text: "Aller chercher les resultats cliniques", done: true}
+    {text: "Aller chercher les resultats cliniques", done: true},
 ]
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // on arrete le rechargement de la page
+    const todoText = input.value; // je recupere le text dans le zone de saisie du input
+    input.value = '';
+
+    const newTodo = { text: todoText, done: false }
+    todos.push(newTodo); // on ajoute cette nouvelle tache dans la liste des taches todos
+    displayTodos();
+})
+
 
 // fonction flechee qui va creer et remplir une balise <li>
 const createTodoElement = (todo) => {
-    const li = document.createElement("li");
+    const li = document.createElement('li');
     li.classList.add("d-flex", "align-items-start");
     li.innerHTML = `
         <span class="todo ${todo.done ? 'done' : ''}></span>
@@ -19,7 +32,6 @@ const createTodoElement = (todo) => {
         <button class="btn btn-primary mx-2">Editer</button>
         <button class="btn btn-danger mx-2">Supprimer</button>
     `
-
     return li;
 }
 
